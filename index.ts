7032ac5f7cf2cfebabe3c7c6f3c44f93be3ed86d
Ryan <ryancollicutt22@gmail.com>
2021-02-20 17:09:18 -0800

    got rid of listen

--- index.ts
+++ index.ts
@@ -6,13 +6,12 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.get("/", (req, res) => res.render("index", {alot: "a lot"}));
-app.listen(8080, () => {
-	console.log("Node application listening on port 8080");
-}); 
 
+
+let geoArrayX: any[] = [];
 
 Geo.build().then((geoObj: Geo) => {
     geoObj.geometry.forEach((obj: any) => {
-        console.log(obj.geometry.x, obj.geometry.y)
+        geoArrayX.push({x: obj.geometry.x, y: obj.geometry.y});
     })
 });
