152964b86ebcb727d79d1a4e1ff4b865836b7f39
Ryan <ryancollicutt22@gmail.com>
2021-02-21 06:04:58 -0800

    little changes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.get("/", (req, res) => res.render("index", {}));
-app.get("\\?map=Open+the+map#", (req, res) => res.render("Map", {}));
+app.get("#", (req, res) => res.render("Map", {}));
 app.listen(port, () => {
 	console.log(`Node application listening on port ${port}`);
 }); 
