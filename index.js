952c659b7853f709f7b2269b2df7a8cc5b2c0168
Ryan <ryancollicutt22@gmail.com>
2021-02-20 22:03:18 -0800

    updated placement

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 const express = require("express");
 const router = include('routes/router');
-app.use('/',router);
+
 const app = express()
 const port = process.env.PORT || 8080;
 app.use(express.urlencoded({extended: false}));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
-
+app.use('/',router);
 app.listen(port, () => {
 	console.log(`Node application listening on port ${port}`);
 }); 
