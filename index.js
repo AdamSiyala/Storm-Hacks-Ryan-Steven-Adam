bfb95ca7c10fb64ffd3b9d182659d8e8ff86ea52
Ryan <ryancollicutt22@gmail.com>
2021-02-20 22:24:57 -0800

    got rid of oruter

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 const express = require("express");
-const router = 'routes/router';
+
 
 const app = express()
 const port = process.env.PORT || 8080;
 app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
-app.use('/',router);
+app.get("/", (req, res) => res.render("index", {alot: "a lot"}));
 app.listen(port, () => {
 	console.log(`Node application listening on port ${port}`);
 }); 
