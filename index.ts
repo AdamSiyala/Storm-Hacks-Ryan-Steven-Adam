e9373efefedb7494935ba954a2e1837bc5ca6d03
Ryan <ryancollicutt22@gmail.com>
2021-02-20 17:32:03 -0800

    added more express properties

--- index.ts
+++ index.ts
@@ -1,13 +1,14 @@
 
 import express from "express";
 import { Geo } from "./geo"
-const app = express();
-
+const app = express()
+const port = process.env.PORT || 8080;
+app.use(express.urlencoded({extended: false}));
 app.set('view engine', 'ejs');
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("index", {alot: "a lot"}));
-app.listen(process.env.PORT, () => {
-	console.log(`Node application listening on port ${process.env.PORT}`);
+app.listen(port, () => {
+	console.log(`Node application listening on port ${port}`);
 }); 
 
 let geoArrayX: any[] = [];
