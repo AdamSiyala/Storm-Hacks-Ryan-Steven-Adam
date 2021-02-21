c3f5f1b263e4210f874f4414a312cfaeea084592
Ryan <ryancollicutt22@gmail.com>
2021-02-20 22:21:04 -0800

    urlencode true

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const router = 'routes/router';
 
 const app = express()
 const port = process.env.PORT || 8080;
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use('/',router);
