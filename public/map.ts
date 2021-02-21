c2fb3a8d99803aafe57a20e7f4a2fbc18e3a04d6
Ryan <ryancollicutt22@gmail.com>
2021-02-21 01:27:06 -0800

    mvp

--- public/map.ts
+++ public/map.ts
@@ -1,23 +1,23 @@
-import { geoArray, GeoFeatures } from '../geo';
+
 
 
 interface LatLng {
     lat: number,
     lng: number
 }
-var location: LatLng = { lat: 49.24890826962529, lng: -122.98049837083043 }
-const geoLoc = navigator.geolocation
+var locat: LatLng = { lat: 49.24890826962529, lng: -122.98049837083043 }
+// const geoLoc = navigator.geolocation
 
-geoLoc.getCurrentPosition((position) => {
-    if (position) location = { lat: position.coords.latitude, lng: position.coords.longitude }
-});
+// geoLoc.getCurrentPosition((position) => {
+//     if (position) location = { lat: position.coords.latitude, lng: position.coords.longitude }
+// });
 
 class GeoMap {
     private _googleMaps: google.maps.Map;
     constructor() {
         console.log("yes")
         this._googleMaps = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-          center: location,
+          center: locat,
           zoom: 10,
         });
       }
