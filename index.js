b85f2337d0bc220eb1296db948bab15de2695770
Ryan <ryancollicutt22@gmail.com>
2021-02-20 18:40:38 -0800

    changed procfile - i think i got it

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const express = require("express");
+
+
+const app = express()
+const port = process.env.PORT || 8080;
+app.use(express.urlencoded({extended: false}));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("index", {alot: "a lot"}));
+app.listen(port, () => {
+	console.log(`Node application listening on port ${port}`);
+}); 
+
+
+
