5e8bfb1cd344a45dafdd738aa15f2c86cdbff29b
Ryan <ryancollicutt22@gmail.com>
2021-02-20 22:22:24 -0800

    router

--- routes/router.js
+++ routes/router.js
@@ -2,4 +2,4 @@ const router = require('express').Router();
 
 
 
-app.get("/", (req, res) => res.render("index", {alot: "a lot"}));
+router.get("/", (req, res) => res.render("index", {alot: "a lot"}));
