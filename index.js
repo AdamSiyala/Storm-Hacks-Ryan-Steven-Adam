9d0a9b73f6e3a96d9e8a5976d145d8a41c1412fb
Ryan <ryancollicutt22@gmail.com>
2021-02-20 22:31:49 -0800

    change 40

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 const port = process.env.PORT || 8080;
 app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 app.get("/", (req, res) => res.render("index", {alot: "a lot"}));
 app.listen(port, () => {
 	console.log(`Node application listening on port ${port}`);
