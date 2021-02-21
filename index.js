1843383a6f525bc47ef71a5b33031202a9e637fa
Ryan <ryancollicutt22@gmail.com>
2021-02-20 18:30:43 -0800

    changed ts file to js file for server

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var geo_1 = require("./geo");
-var app = express_1["default"]();
-var port = process.env.PORT || 8080;
-app.use(express_1["default"].urlencoded({ extended: false }));
+
+const express = require("express");
+
+
+const app = express()
+const port = process.env.PORT || 8080;
+app.use(express.urlencoded({extended: false}));
 app.set('view engine', 'ejs');
-app.use(express_1["default"].static(__dirname + "/public"));
-app.get("/", function (req, res) { return res.render("index", { alot: "a lot" }); });
-app.listen(port, function () {
-    console.log("Node application listening on port " + port);
-});
-var geoArrayX = [];
-geo_1.Geo.build().then(function (geoObj) {
-    geoObj.geometry.forEach(function (obj) {
-        geoArrayX.push({ x: obj.geometry.x, y: obj.geometry.y });
-    });
-});
+app.use(express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("index", {alot: "a lot"}));
+app.listen(port, () => {
+	console.log(`Node application listening on port ${port}`);
+}); 
+
+
+
